fb6ea6fe73364c4eaf2f07fe839739e10cb6fa07
Felipe Rosa Reis <felipe.reis@leadforce.com.br>
2021-04-19 08:54:16 -0300

    - Diagramação do topo e da seção de conteúdo e formulário.

---
+++ index.php
@@ -0,0 +1,77 @@
+<?php include("./includes/header.php") ?>
+
+<div class="top-background"></div>
+<header class="top-header">
+
+    <div class="container">
+        <p class="text-center color-white">uma seleção de produtos</p>
+        <h1 class="text-center color-white">especial para você</h1>
+
+        <p class="text-center color-white">Todos os produtos desta lista foram selecionados a partir da sua navegação. Aproveite!</p>
+
+        <div class="menu">
+            <a href="">
+                <button>Conheça a Linx</button>
+            </a>
+            <a href="">
+                <button>Ajude o Algoritmo</button>
+            </a>
+            <a href="">
+                <button>Seus produtos</button>
+            </a>
+            <a href="">
+                <button>Compartilhe</button>
+            </a>
+
+        </div>
+    </div>
+</header>
+
+<section class="form">
+    <div class="container">
+        <div class="flex-box">
+            <div class="box-70 mb-30">
+                <h4 class="title">Ajude o algoritmo a ser mais certeiro</h4>
+
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, tempora ipsa harum temporibus at quis consequatur nam possimus perferendis aspernatur cupiditate sed assumenda nulla eos illo veniam, quod quia facere reiciendis maxime nemo, veritatis placeat suscipit? Officia veritatis corporis alias ratione distinctio nam rem ex vitae ipsam optio vero rerum voluptatibus ducimus nemo officiis ea, deserunt magnam aliquam dicta cumque soluta id est quia? Harum magnam nulla velit odit, totam nihil quia, aspernatur recusandae error sunt eaque, quam eum dicta consequatur modi quas vel distinctio! Pariatur, consectetur. Quae impedit, accusantium, sint, error qui dolores deleniti earum vero quis modi distinctio!
+                    <br>
+                    <br>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure quam aspernatur eligendi harum officiis enim delectus saepe fugit ea. Libero quis unde dolorum nobis cum? Quod ab cum voluptates error? Ut, itaque similique! Consequuntur sequi labore ut, iste sunt quibusdam, aperiam quo dolore itaque quis accusantium necessitatibus cum nesciunt quae.
+                </p>
+            </div>
+            <div class="box-30 mb-30">
+                <form action="">
+                    <div class="input-group">
+                        <label for="#name">Seu nome</label>
+                        <input type="text" name="name" class="full-width">
+                    </div>
+                    <div class="input-group">
+                        <label for="#name">E-mail</label>
+                        <input type="email" name="email" class="full-width">
+                    </div>
+                    <div class="input-group">
+                        <label for="#name">CPF</label>
+                        <input type="text" name="name" class="full-width">
+                    </div>
+    
+                    <div class="input-group">
+                        <label for="">
+                            <input type="radio" name="gender" value="m"> Masculino
+                        </label>
+                        
+                        <label for="">
+                            <input type="radio" name="gender" value="f"> Feminino
+                        </label>
+                    </div>
+
+                    <button class="btn-form">Enviar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+<?php include("./includes/footer.php") ?>
