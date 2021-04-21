71120ac103cfa5ac40472a9fc072a769705fe171
Felipe Rosa Reis <felipe.reis@leadforce.com.br>
2021-04-21 17:48:30 -0300

    - Implementado o template de e-mail e o envio deste template via e-mail com a library PHPMailer.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,16 @@ $(document).ready(function(){
 
     loadMore();
 
+    if (success === "1")    // Exibe um alert se o e-mail tiver sido enviado.
+    {
+        alert('E-mail enviado com sucesso!');
+    }
 });
 
 // Função que faz a paginação.
 function loadMore()
 {
-    let pageUrl = $('.btn-load-more').data('page');
+    let pageUrl = $('.btn-load-more').data('page');     // Pega a url da primeira págnia de produtos contida no botão "Ainda mais produtos aqui";
     let html = "";
 
     $.ajax({
@@ -21,6 +25,7 @@ function loadMore()
                     let price       = currencyFormat(val.price);
                     let installment = currencyFormat(val.installments.value);
                     
+                    // Monta os blocos dos produtos a serem carregados na página.
                     html += "<li class='flex'>";
                     html += "<div class='box-img'>";
                     html += "<img src='" + val.image + "' title='" + val.name + "'>";
@@ -39,17 +44,17 @@ function loadMore()
                 
                 $('.products-list').append(html);
                 
-                
+                // Atualiza a url do botão para carregar a próxima página.
                 $('.btn-load-more').data('page', 'https://'+data.nextPage);
 
             }
         })
         .fail(function(data){
             console.log('fail...');
-
         });
 }
 
+// Formata uma string de valor em moeda.
 function currencyFormat(value) {
     let result = value.toLocaleString('pt-BR', {
         minimumFractionDigits: 2
--- includes/share.php
+++ includes/share.php
@@ -5,17 +5,17 @@
         <div class="share-container">
             <p class="text-center full-width">Quer que seus amigos também ganhem a lista personalizada deles? Preencha agora!</p>
 
-            <form action="" class="full-width flex">
+            <form action="helper/mail.php" method="POST" class="full-width flex">
                 <div class="box-50">
                     <div class="input-group">
                         <label for="#name">Nome do seu amigo:</label>
-                        <input type="text" name="name" class="full-width">
+                        <input type="text" name="name" class="full-width" required minlength="3">
                     </div>
                 </div>
                 <div class="box-50">
                     <div class="input-group">
                         <label for="#email">E-mail dele:</label>
-                        <input type="email" name="email" class="full-width">
+                        <input type="email" name="email" class="full-width" required>
                     </div>
                 </div>
 
